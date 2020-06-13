f0aaa95fc90f516a9676bc9d65f2e3dc05623718
mark <marx.design@icloud.com>
2020-06-13 21:10:44 +0500

    add cqrs commands

--- labs/src/modules/labs/lab.resolver.ts
+++ labs/src/modules/labs/lab.resolver.ts
@@ -1,15 +1,22 @@
 import { OnModuleInit, Inject, Logger } from '@nestjs/common';
-import { Resolver, Query, Args, ResolveReference, Mutation } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Args,
+  ResolveReference,
+  Mutation,
+} from '@nestjs/graphql';
 import { ClientGrpc } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Lab } from './models/lab.model';
 import { LabRepository } from './lab.repository';
-import { CommandBus } from '@nestjs/cqrs'
+import { CommandBus } from '@nestjs/cqrs';
 import { ChangeLabIdCommand } from './commands/impl/change-lab-id.command';
 import { InsertLabDto } from './models/dto/insert-lab.dto';
 import { CreateLabCommand } from './commands/impl/create-lab.command';
 import { CreateLabDto } from './models/dto/create-lab.dto';
+import { UpdateLabCommand } from './commands/impl/update-lab.command';
 
 interface LabService {
   findAllLabs(data: number): Observable<Lab>;
@@ -24,7 +31,7 @@ export class LabResolver implements OnModuleInit {
   constructor(
     private readonly labRepository: LabRepository,
     @Inject('LAB_PACKAGE') private readonly client: ClientGrpc,
-    private readonly commandBus: CommandBus
+    private readonly commandBus: CommandBus,
   ) {}
 
   onModuleInit() {
@@ -46,7 +53,9 @@ export class LabResolver implements OnModuleInit {
     return this.labService.findAllLabs(1).pipe(
       map(async lab => {
         const newLab = await this.labRepository.migrationCreateLab(lab);
-        await this.commandBus.execute(new ChangeLabIdCommand(newLab.id, lab.id))
+        await this.commandBus.execute(
+          new ChangeLabIdCommand(newLab.id, lab.id),
+        );
         this.logger.verbose(newLab);
         return newLab;
       }),
@@ -54,11 +63,18 @@ export class LabResolver implements OnModuleInit {
   }
 
   @Mutation(returns => Lab)
-  async createLab(@Args('createLabData') createLabData: CreateLabDto): Promise<Lab> {
-    return await this.commandBus.execute(new CreateLabCommand(createLabData))
+  async createLab(
+    @Args('createLabData') createLabData: CreateLabDto,
+  ): Promise<Lab> {
+    return await this.commandBus.execute(new CreateLabCommand(createLabData));
   }
 
   @Mutation(returns => Lab)
+  async updateLab(
+    @Args('insertLabData') insertLabData: InsertLabDto,
+  ): Promise<Lab> {
+    return await this.commandBus.execute(new UpdateLabCommand(insertLabData));
+  }
 
   @ResolveReference()
   async resolverReference(reference: { __typename: string; id: string }) {
