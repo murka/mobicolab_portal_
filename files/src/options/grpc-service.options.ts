c52d761dcdfd72bba1791e22450fbeaef83c1a69
mark <marx.design@icloud.com>
2020-09-03 15:19:00 +0500

    proto

--- files/src/options/grpc-service.options.ts
+++ files/src/options/grpc-service.options.ts
@@ -5,10 +5,9 @@ export const grpcServiceOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
     url: '0.0.0.0:50051',
-    package: ['template', 'files', 'files_service'],
+    package: ['template', 'files_service'],
     protoPath: [
       join(__dirname, './protos/template.proto'),
-      join(__dirname, '../../../proto/files.proto'),
       '/proto/files/files.proto',
     ],
   },
