412ba526ca55deb223a1ae663f0a6a6e483bae30
mark <marx.design@icloud.com>
2020-09-03 12:08:51 +0500

    again

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
-  baseURL: 'http://157.230.102.161:3000/api/',
-  API_URI: 'http://157.230.102.161:3333/nestapi/',
-  GQL_URI_FILES: 'http://157.230.102.161:2000/graphql/',
-  WS_URI_FILES: "ws://157.230.102.161:3006/graphql",
+  baseURL: "http://172.17.0.1:3000/api/",
+  API_URI: "http://172.17.0.1:3333/nestapi/",
+  GQL_URI: "http://172.17.0.1:2000/graphql",
+  GQL_URI_SUB: "http://172.17.0.1:3060/graphql/",
+  WS_URI_FILES: "ws://172.17.0.1:3006/graphql",
+  URI: "http://172.17.0.1/api",
 };
