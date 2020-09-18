3deecf62b268c45ae6115be5f17642e14d58e9a8
mark <marx.design@icloud.com>
2020-09-18 10:07:28 +0500

    some rearrange

---
+++ client/src/app/acts/acts-list/list-items/acts-table/acts-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acts-table',
+  templateUrl: './acts-table.component.html',
+  styleUrls: ['./acts-table.component.scss']
+})
+export class ActsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
